799ae2cb103dbe41fe7ba59e25561e05f3a6942e
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-18 13:31:24 +0100

    Improved map icons

--- android/app/src/main/kotlin/pl/kwasow/ui/screens/modules/location/FlamingoMapView.kt
+++ android/app/src/main/kotlin/pl/kwasow/ui/screens/modules/location/FlamingoMapView.kt
@@ -1,19 +1,23 @@
 package pl.kwasow.ui.screens.modules.location
 
 import android.location.Location
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerComposable
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import dev.chrisbanes.haze.HazeState
@@ -21,7 +25,6 @@ import dev.chrisbanes.haze.hazeSource
 import org.koin.androidx.compose.koinViewModel
 import pl.kwasow.R
 import pl.kwasow.data.UserLocation
-import pl.kwasow.utils.bitmapDescriptorFromVector
 
 // ====== Public composables
 @Composable
@@ -86,20 +89,32 @@ fun FlamingoMapView(
 private fun CurrentLocationMarker(location: Location) {
     val markerState = rememberMarkerState(position = LatLng(location.latitude, location.longitude))
 
-    Marker(
+    MarkerComposable(
         state = markerState,
         title = stringResource(id = R.string.module_location_your_location),
-        icon = bitmapDescriptorFromVector(R.drawable.ic_current_location),
-    )
+        onClick = { false },
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_current_location),
+            contentDescription = stringResource(id = R.string.module_location_your_location),
+            modifier = Modifier.size(16.dp),
+        )
+    }
 }
 
 @Composable
 private fun PersonMarker(location: UserLocation) {
     val markerState = rememberMarkerState(position = LatLng(location.latitude, location.longitude))
 
-    Marker(
+    MarkerComposable(
         state = markerState,
         title = location.userName,
-        icon = bitmapDescriptorFromVector(R.drawable.ic_map_user_marker),
-    )
+        onClick = { false },
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_map_user_marker),
+            contentDescription = location.userName,
+            modifier = Modifier.size(16.dp),
+        )
+    }
 }
