1876c69829129e14072a7c18a03d3492966b587a
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-19 12:18:57 +0100

    Forced locale for Karonia flavor

--- android/app/src/main/kotlin/pl/kwasow/MainActivity.kt
+++ android/app/src/main/kotlin/pl/kwasow/MainActivity.kt
@@ -1,17 +1,22 @@
 package pl.kwasow
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
 import androidx.core.view.WindowCompat
 import org.koin.compose.KoinContext
+import pl.kwasow.data.BuildFlavors
 import pl.kwasow.ui.App
 import pl.kwasow.ui.theme.FlamingoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    // ====== Public methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupKaroniaDefaults()
         setupSystemBars()
 
         setContent {
@@ -23,7 +28,17 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    // ====== Private methods
     private fun setupSystemBars() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+
+    private fun setupKaroniaDefaults() {
+        if (BuildConfig.FLAVOR != BuildFlavors.KARONIA.flavorName) {
+            return
+        }
+
+        val appLocale = LocaleListCompat.forLanguageTags("pl")
+        AppCompatDelegate.setApplicationLocales(appLocale)
     }
 }
