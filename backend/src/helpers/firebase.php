f677520b32dec0aa8135da72e363058ed8fae9a6
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-22 18:01:27 +0100

    Added implementation of sending to tokens

--- backend/src/helpers/firebase.php
+++ backend/src/helpers/firebase.php
@@ -27,5 +27,27 @@ function sendTopicFirebaseMessage($topic, $data)
 }
 
 function sendUserFirebaseMessage($userId, $data, $connection) {
+    $stmt = mysqli_prepare(
+        $conn,
+        'SELECT token FROM FirebaseTokens WHERE user_id = ?'
+    );
+    mysqli_stmt_bind_param($stmt, 'i', $userId);
+    mysqli_stmt_execute($stmt);
 
+    $tokens = $stmt->get_result();
+
+    if ($tokens == null) {
+        return null;
+    }
+
+    $messaging = $GLOBALS['firebaseMessaging'];
+
+    $messages = [];
+    foreach ($tokens as $token) {
+        $message = CloudMessage::new()
+            ->withData($data)
+            ->toToken($token);
+    }
+
+    return $messaging->sendAll($messages);
 }
