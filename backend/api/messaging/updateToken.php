4a56f2551878eb83551d6c333f3bb154ef9f6357
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-22 15:32:58 +0100

    Added backend firebase token update logic

--- backend/api/messaging/updateToken.php
+++ backend/api/messaging/updateToken.php
@@ -18,23 +18,33 @@ if ($user !== null) {
 $postData = json_decode(file_get_contents('php://input'), true);
 $token = $postData['token'];
 $debug = $postData['debug'];
+$userId = $user->getId;
 
-$id = $user->getId();
-
-// Add token to database
 $stmt = mysqli_prepare(
     $conn,
-    'INSERT INTO FirebaseTokens VALUES(NULL, ?, ?, ?, ?)'
+    'SELECT id FROM FirebaseTokens WHERE token = ?'
 );
-mysqli_stmt_bind_param(
-    $stmt,
-    'iisi',
-    $id,
-    time(),
-    $token,
-    $debug
-);
+mysqli_stmt_bind_param($stmt, 's', $token);
 mysqli_stmt_execute($stmt);
+
+$result = $stmt->get_result();
+
+// Update if exists, else add
+if (($row = mysqli_fetch_row($result)) != null) {
+    $stmt = mysqli_prepare(
+        $conn,
+        'UPDATE FirebaseTokens SET time_stamp = ? WHERE id = ?'
+    );
+    mysqli_stmt_bind_param($stmt, 'ii', time(), $row[0]);
+    mysqli_stmt_execute($stmt);
+} else {
+    $stmt = mysqli_prepare(
+        $conn,
+        'INSERT INTO FirebaseTokens VALUES(NULL, ?, ?, ?, ?)'
+    );
+    mysqli_stmt_bind_param($stmt, 'iisi', $userId, time(), $token, $debug);
+    mysqli_stmt_execute($stmt);
+}
 
 mysqli_close($conn);
 exit();
