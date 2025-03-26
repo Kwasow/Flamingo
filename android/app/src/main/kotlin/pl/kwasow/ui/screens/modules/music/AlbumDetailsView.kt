c3512c749fb9b9bc1e1b59c77d2949f74839e431
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-26 22:13:55 +0100

    Replaced toast with proper UI in album view

--- android/app/src/main/kotlin/pl/kwasow/ui/screens/modules/music/AlbumDetailsView.kt
+++ android/app/src/main/kotlin/pl/kwasow/ui/screens/modules/music/AlbumDetailsView.kt
@@ -3,16 +3,24 @@ package pl.kwasow.ui.screens.modules.music
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.koinViewModel
 import pl.kwasow.R
@@ -26,9 +34,12 @@ fun AlbumDetailsView(albumUuid: String) {
     val viewModel = koinViewModel<MusicModuleViewModel>()
     val navigation = LocalFlamingoNavigation.current
 
-    val album =
-        viewModel.getAlbumByUuid(albumUuid)
-            ?: return albumNotFound(navigation.navigateBack, LocalContext.current)
+    val album = viewModel.getAlbumByUuid(albumUuid)
+
+    if (album == null) {
+        AlbumNotFound(uuid = albumUuid)
+        return
+    }
 
     Scaffold(
         topBar = {
@@ -54,10 +65,33 @@ fun AlbumDetailsView(albumUuid: String) {
 }
 
 // ====== Private composables
-private fun albumNotFound(
-    onBackPressed: () -> Unit,
-    context: Context,
-) {
-    Toast.makeText(context, R.string.something_went_wrong, Toast.LENGTH_LONG).show()
-    onBackPressed()
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AlbumNotFound(uuid: String) {
+    val navigation = LocalFlamingoNavigation.current
+
+    Scaffold(
+        topBar = {
+            FlamingoTopAppBar(
+                title = stringResource(R.string.error),
+                onBackPressed = navigation.navigateBack,
+            )
+        },
+    ) { paddingValues ->
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            SelectionContainer {
+                Text(
+                    stringResource(id = R.string.module_music_unknown_album_uuid, uuid),
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic
+                )
+            }
+        }
+    }
 }
