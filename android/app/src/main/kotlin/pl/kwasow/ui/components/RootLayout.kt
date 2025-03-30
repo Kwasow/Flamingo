0e17edb870aed3068d25ecd2b33729f3be1e208f
Karol Wąsowski <wasowski02@protonmail.com>
2025-03-30 13:11:43 +0200

    Provide bottom bar height through a composition local

--- android/app/src/main/kotlin/pl/kwasow/ui/components/RootLayout.kt
+++ android/app/src/main/kotlin/pl/kwasow/ui/components/RootLayout.kt
@@ -2,11 +2,17 @@ package pl.kwasow.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import pl.kwasow.ui.composition.LocalBottomBarHeight
 
 // ====== Public composables
 @Composable
@@ -14,15 +20,24 @@ fun RootLayout(
     bottomBar: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
+    var bottomBarHeight by remember { mutableStateOf(0.dp) }
+
     Box {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            content()
+        CompositionLocalProvider(LocalBottomBarHeight provides bottomBarHeight) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                content()
+            }
         }
 
         Box(
-            modifier = Modifier.align(Alignment.BottomCenter),
+            modifier =
+                Modifier
+                    .align(Alignment.BottomCenter)
+                    .onGloballyPositioned { coordinates ->
+                        bottomBarHeight = coordinates.size.height.dp
+                    },
         ) {
             bottomBar()
         }
---
+++ android/app/src/main/kotlin/pl/kwasow/ui/composition/LocalBottomBarHeight.kt
@@ -0,0 +1,6 @@
+package pl.kwasow.ui.composition
+
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.dp
+
+val LocalBottomBarHeight = compositionLocalOf { 0.dp }
