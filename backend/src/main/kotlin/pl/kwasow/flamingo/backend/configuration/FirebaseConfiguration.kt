2ab154e97fd50c8a96c286460736c0ad857511ad
Karol Wąsowski <wasowski02@protonmail.com>
2025-08-21 22:14:04 +0200

    Stash work

--- backend/src/main/kotlin/pl/kwasow/flamingo/backend/configuration/FirebaseConfiguration.kt
+++ backend/src/main/kotlin/pl/kwasow/flamingo/backend/configuration/FirebaseConfiguration.kt
@@ -1,4 +1,4 @@
-package pl.kwasow.flamingo.backend
+package pl.kwasow.flamingo.backend.configuration
 
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class FirebaseConfig {
+class FirebaseConfiguration {
 
     @Bean
     fun firebaseApp(): FirebaseApp {
