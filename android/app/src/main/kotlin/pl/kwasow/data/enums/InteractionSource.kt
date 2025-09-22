1fe5ccd4837b7d5330ca89a871a1ca00d45e5eca
Karol Wąsowski <wasowski02@protonmail.com>
2025-09-22 22:55:48 +0200

    Release 2.5.1

--- android/app/src/main/kotlin/pl/kwasow/data/enums/InteractionSource.kt
+++ android/app/src/main/kotlin/pl/kwasow/data/enums/InteractionSource.kt
@@ -1,5 +1,9 @@
 package pl.kwasow.data.enums
 
+import androidx.annotation.Keep
+
+// For some reason R8 removes enum classes, so this annotation is necessary to prevent it
+@Keep
 enum class InteractionSource {
     User,
     Notification,
