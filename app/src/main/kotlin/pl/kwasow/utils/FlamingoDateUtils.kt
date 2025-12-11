4b1d810c2f261d1bf162fdb3d0193af33a2c5f65
Karol Wąsowski <wasowski02@protonmail.com>
2025-12-11 21:43:57 +0100

    Removed unused function

--- app/src/main/kotlin/pl/kwasow/utils/FlamingoDateUtils.kt
+++ app/src/main/kotlin/pl/kwasow/utils/FlamingoDateUtils.kt
@@ -1,11 +1,8 @@
 package pl.kwasow.utils
 
 import android.text.format.DateUtils
-import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
-import java.time.Month
-import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -15,30 +12,6 @@ object FlamingoDateUtils {
     private val textDateFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG)
 
     // ====== Public methods
-    fun timestampToString(timestamp: Long): String {
-        val instant = Instant.ofEpochMilli(timestamp)
-        val date = LocalDateTime.ofInstant(instant, ZoneId.systemDefault())
-
-        val month =
-            when (date.month) {
-                Month.JANUARY -> "stycznia"
-                Month.FEBRUARY -> "lutego"
-                Month.MARCH -> "marca"
-                Month.APRIL -> "kwietnia"
-                Month.MAY -> "maja"
-                Month.JUNE -> "czerwca"
-                Month.JULY -> "lipca"
-                Month.AUGUST -> "sierpnia"
-                Month.SEPTEMBER -> "września"
-                Month.OCTOBER -> "października"
-                Month.NOVEMBER -> "listopada"
-                Month.DECEMBER -> "grudnia"
-                else -> NullPointerException("date.month cannot be null")
-            }
-
-        return "${date.dayOfMonth} $month ${date.year}"
-    }
-
     fun localDateToString(date: LocalDateTime): String = localDateToString(date.toLocalDate())
 
     fun localDateToString(date: LocalDate): String = textDateFormatter.format(date)
