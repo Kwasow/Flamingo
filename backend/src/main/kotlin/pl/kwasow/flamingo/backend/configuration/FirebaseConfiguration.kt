76e56e7c9ecc31b1bb8a1bfcbf183db04ea89623
Karol Wąsowski <wasowski02@protonmail.com>
2025-12-12 09:50:37 +0100

    Configure backend to make running in docker easier (#65)

--- backend/src/main/kotlin/pl/kwasow/flamingo/backend/configuration/FirebaseConfiguration.kt
+++ backend/src/main/kotlin/pl/kwasow/flamingo/backend/configuration/FirebaseConfiguration.kt
@@ -1,8 +1,6 @@
 package pl.kwasow.flamingo.backend.configuration
 
-import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.messaging.FirebaseMessaging
 import org.springframework.context.annotation.Bean
@@ -12,24 +10,7 @@ import org.springframework.context.annotation.Configuration
 class FirebaseConfiguration {
     // ====== Beans
     @Bean
-    fun firebaseApp(): FirebaseApp {
-        val serviceAccount =
-            System
-                .getenv("FIREBASE_CONFIG")
-                .substring(1)
-                .dropLast(1)
-                .byteInputStream()
-
-        println(serviceAccount)
-
-        val options =
-            FirebaseOptions
-                .builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build()
-
-        return FirebaseApp.initializeApp(options)
-    }
+    fun firebaseApp(): FirebaseApp = FirebaseApp.initializeApp()
 
     @Bean
     fun firebaseAuth(firebaseApp: FirebaseApp): FirebaseAuth = FirebaseAuth.getInstance(firebaseApp)
